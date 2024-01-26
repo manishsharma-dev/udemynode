b8fa9b7fd0686ec13d6c88c87250f066597ce8cb
Manish Sharma <sharmakmanish1@gmail.com>
2024-01-26 22:39:20 +0530

    error handling

--- app.js
+++ app.js
@@ -3,17 +3,38 @@ const app = express();
 app.use(express.json());
 const dotenv = require('dotenv');
 
+
+//Handling uncaught exceptions
+
+process.on('uncaughtException', err => {
+    console.log(`Error : ${err.message}`);
+    console.log(`Shutting down server due to uncaught exception`);
+    process.exit(1);
+})
+
 const connectDatabase = require("./config/database");
 
 dotenv.config({ path: './config/config.env' });
 
 connectDatabase(); // connect DB
+const errorMiddleware = require('./middlewares/error');
 
 const jobs = require('./routes/jobs');
 
 app.use('/api/v1', jobs);
 
+app.use(errorMiddleware) //middleware to handle error
+
 const port = process.env.PORT || 3000
-app.listen(port || 3000, () => {
+const server = app.listen(port || 3000, () => {
     console.log(`listening on port ${port} in ${process.env.NODE_ENV} mode`);
 })
+
+process.on('unhandledRejection', err => {
+    console.log(`Error: ${err.message}`);
+    console.log('Shutting down the server due to handled promise rejection');
+    server.close(() => {
+        process.exit(1);
+    });
+});
+
--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,34 +1,34 @@
 const Job = require('../models/jobs');
 const geoCoder = require('../utils/geocoder');
+const ErrorHandler = require('../utils/errorhandler');
+const catchAsyncErrors = require('../middlewares/catchAsyncErrors');
+
 
 //Get All Jobs => /api/v1/jobs
-exports.getJobs = async (req, res, next) => {
+exports.getJobs = catchAsyncErrors(async (req, res, next) => {
     const jobs = await Job.find();
     res.status(200).json({
         success: true,
         results: jobs.length,
         data: jobs
     })
-}
+})
 
 //Creates a new job => /api/v1/jobs/new
-exports.newJob = async (req, res, next) => {
+exports.newJob = catchAsyncErrors(async (req, res, next) => {
     const job = await Job.create(req.body);
     res.status(200).json({
         success: true,
         message: 'Job created successfully',
         data: job
     });
-}
+})
 
 //Update job => /api/v1/jobs/:id
-exports.updateJob = async (req, res, next) => {
+exports.updateJob = catchAsyncErrors(async (req, res, next) => {
     let job = await Job.findById(req.params.id);
     if (!job) {
-        return res.status(404).json({
-            success: false,
-            message: 'Job not found',
-        })
+        return next(new ErrorHandler('Job not found', 404));
     }
     job = await Job.findByIdAndUpdate(req.params.id, req.body, {
         new: true,
@@ -40,11 +40,11 @@ exports.updateJob = async (req, res, next) => {
         message: 'Job successfully updated',
         data: job
     })
-}
+})
 
 //Get a single job with id and slug => /api/v1/jobs/:id/:slug
 
-exports.getJob = async (req, res, next) => {
+exports.getJob = catchAsyncErrors(async (req, res, next) => {
     const job = await Job.find({ $and: [{ _id: req.params.id }, { slug: req.params.slug }] });
     if (!job || job.length === 0) {
         return res.status(200).json({
@@ -56,10 +56,10 @@ exports.getJob = async (req, res, next) => {
         success: true,
         data: job
     })
-}
+})
 
 // search job within radius => /api/v1/jobs/:zipcode/:distance
-exports.getJobsInRadius = async (req, res, next) => {
+exports.getJobsInRadius = catchAsyncErrors(async (req, res, next) => {
     const { zipcode, distance } = req.params;
     //Getting Lat & Lng from geocoder
     const loc = await geoCoder.geocode(zipcode);
@@ -77,10 +77,10 @@ exports.getJobsInRadius = async (req, res, next) => {
         results: jobs.length,
         data: jobs
     })
-}
+})
 
 // Delete a job => /api/v1/jobs/:id
-exports.deleteJob = async (req, res, next) => {
+exports.deleteJob = catchAsyncErrors(async (req, res, next) => {
     let job = await Job.findById(req.params.id);
     if (!job) {
         return res.status(404).json({
@@ -95,11 +95,11 @@ exports.deleteJob = async (req, res, next) => {
         success: true,
         message: 'Job successfully removed'
     })
-}
+})
 
 //get stats about a topic(job) => /api/v1/stats/:topic
 
-exports.jobStats = async (req, res, next) => {
+exports.jobStats = catchAsyncErrors(async (req, res, next) => {
     try {
         const stats = await Job.aggregate([
             {
@@ -107,12 +107,12 @@ exports.jobStats = async (req, res, next) => {
             },
             {
                 $group: {
-                    _id: {$toUpper : '$experience'},
+                    _id: { $toUpper: '$experience' },
                     totalJobs: { $sum: 1 },
-                    avgPosition: { $avg : '$positions' },
+                    avgPosition: { $avg: '$positions' },
                     avgSalary: { $avg: '$salary' },
-                    minSalary: {$min : '$salary' },
-                    maxSalary: {$max : '$salary' },
+                    minSalary: { $min: '$salary' },
+                    maxSalary: { $max: '$salary' },
                 }
             }
         ])
@@ -136,4 +136,4 @@ exports.jobStats = async (req, res, next) => {
 
 
 
-}
+})
