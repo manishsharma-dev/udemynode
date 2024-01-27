9bf159869ec64bea53939de0ac7796d18bd5102d
Manish Sharma <sharmakmanish1@gmail.com>
2024-01-27 13:34:58 +0530

    sorting though params

--- utils/apiFilters.js
+++ utils/apiFilters.js
@@ -7,10 +7,24 @@ class APIFilters {
     filter() {
         const queryCopy = { ...this.queryStr };
 
+        // removing fields from query
+        const removeFields = ['sort'];
+        removeFields.forEach(element => delete queryCopy[element]);
+
         // Advance filte using : lt,lte,gt,gte
         let queryStr = JSON.stringify(queryCopy);
         queryStr = queryStr.replace(/\b(gt|gte|lt|lte|in)\b/g, match => `$${match}`);
         this.query = this.query.find(JSON.parse(queryStr));
+        return this;
+    }
+
+    sort() {
+        if (this.queryStr.sort) {
+            const sortBy = this.queryStr.sort.split(',').join(' ');
+            this.query = this.query.sort(sortBy);
+        } else {
+            this.query = this.query.sort('-postingDate');
+        }
         return this;
     }
 }
